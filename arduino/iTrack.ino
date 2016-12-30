3bc846af7fc7b037c8657e8e460b509675e7b07a
Anil Kumar <akumar@atheerair.com>
2016-12-30 05:15:13 -0800

    Fix pointer crash in Arduino code.

--- arduino/iTrack.ino
+++ arduino/iTrack.ino
@@ -12,36 +12,22 @@
 SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
 SoftwareSerial *fonaSerial = &fonaSS;
 
-// Hardware serial is also possible!
-//  HardwareSerial *fonaSerial = &Serial1;
-
 Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
-// Have a FONA 3G? use this object type instead
-//Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
-
+boolean DEBUG = false;
 
 void setup() {
-
-  while (! Serial);
-
-  Serial.begin(115200);
-  Serial.println(F("Adafruit FONA 808 & 3G GPS demo"));
-  Serial.println(F("Initializing FONA... (May take a few seconds)"));
+  if(DEBUG) {
+    while (! Serial);
+    Serial.begin(115200);
+  }
 
   fonaSerial->begin(4800);
   if (! fona.begin(*fonaSerial)) {
-    Serial.println(F("Couldn't find FONA"));
+    if(DEBUG) Serial.println(F("Couldn't find FONA"));
     while(1);
   }
-  Serial.println(F("FONA is OK"));
-  // Try to enable GPRS
-  
-
-  Serial.println(F("Enabling GPS..."));
-  fona.enableGPS(true);
-  Serial.println(F("Enabling GPRS..."));
-  fona.enableGPRS(true);
+  if(DEBUG) Serial.println(F("FONA is OK"));
 }
 
 char * append(char * str1, char * str2) {
@@ -57,42 +43,37 @@ void loop() {
   float latitude, longitude, speed_kph, heading, speed_mph, altitude;
 
   // if you ask for an altitude reading, getGPS will return false if there isn't a 3D fix
-  boolean gps_success = fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude);
+  if(DEBUG) Serial.println(F("Enabling GPS..."));
+  fona.enableGPS(true);
+  boolean gps_success = fona.getGPS(&latitude, &longitude, &speed_kph, &heading);
 
   if (gps_success) {
-    
-    char * getURL;
+    if(DEBUG) Serial.println(F("Enabling GPRS..."));
+    fona.enableGPRS(true);
+    String getURL;
     char sLatitude[141];
     char sLongitude[141];
-    char baseURL[100] = "ec2-54-90-96-132.compute-1.amazonaws.com";
     
     dtostrf(latitude, 6, 6, sLatitude);
     dtostrf(longitude, 6, 6, sLongitude);
-
-    getURL = append(baseURL, "/?latitude=");
-    getURL = append(getURL, sLatitude);
-    getURL = append(getURL, "&longitude=");
-    getURL = append(getURL, sLongitude);
+    
+    String baseURL = String("ec2-54-90-96-132.compute-1.amazonaws.com");
+    
+    getURL = baseURL + String("/?latitude=");
+    getURL = getURL + sLatitude;
+    getURL = getURL + String("&longitude=");
+    getURL = getURL + sLongitude;
     
     uint16_t statuscode;
     int16_t length;
 
-    if (!fona.HTTP_GET_start(getURL, &statuscode, (uint16_t *)&length)) {
-          Serial.println("Failed!");
+    if (!fona.HTTP_GET_start((char*)getURL.c_str(), &statuscode, (uint16_t *)&length)) {
+          if(DEBUG) Serial.println("Failed!");
         }
-/*        while (length > 0) {
-          while (fona.available()) {
-            char c = fona.read();
-            loop_until_bit_is_set(UCSR0A, UDRE0);
-            UDR0 = c;
-            length--;
-            if (! length) break;
-          }
-        }*/
-        fona.HTTP_GET_end();
+    fona.HTTP_GET_end();
 
   } else {
-    Serial.println("Waiting for FONA GPS 3D fix...");
+    if(DEBUG) Serial.println("Waiting for FONA GPS 3D fix...");
   }
 }
 
